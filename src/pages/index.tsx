803144c27f9020322ca4ec5dda6bd12731155af5
shawn Wong <jadesure17@gmail.com>
2022-10-29 18:22:03 +1100

    add back

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ const Index: NextPage = () => {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Oldiron666
+          Sign Up
         </Typography>
         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
           <TextField
